4c537d9b78de5f5027f66bf14a607c95f4949f6f
Franz-Josef Grosch <franz-josef.grosch@de.bosch.com>
2020-09-23 10:50:29 +0200

    Additional package names added to codegen tests This was necessary for external C access.
    Signed-off-by: Franz-Josef Grosch <franz-josef.grosch@de.bosch.com>

--- test/blechc/codegeneration/externalImmutable.ext.h
+++ test/blechc/codegeneration/externalImmutable.ext.h
@@ -1,10 +1,10 @@
 #ifndef MY_EXTERNALIMMUTABLE_EXT_H_INCLUDED
 #define MY_EXTERNALIMMUTABLE_EXT_H_INCLUDED
 
-#include "externalImmutable.h"
+#include "blech/externalImmutable.h"
 
 blc_int32 c_name_extVarI8;
 blc_int32 c_name_extVarArr[8];
-struct blc_externalImmutable_S c_name_S2;
+struct blc_blech_externalImmutable_S c_name_S2;
 
 #endif
