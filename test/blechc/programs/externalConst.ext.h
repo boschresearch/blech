b754be336592b8c0fab993d9d899f031d343150e
Franz-Josef Grosch <franz-josef.grosch@de.bosch.com>
2021-03-31 17:49:41 +0200

    Moved tests with external Blech structs. In programs the include hierarchy contained cycles. This is fixed now in modules. Corrected programs tests with external consts. Still to do: correct programs tests with variables and immutables.
    Signed-off-by: Franz-Josef Grosch <franz-josef.grosch@de.bosch.com>

--- test/blechc/programs/externalConst.ext.h
+++ test/blechc/programs/externalConst.ext.h
@@ -1,10 +1,9 @@
 #ifndef MY_EXTERNALCONST_EXT_H_INCLUDED
 #define MY_EXTERNALCONST_EXT_H_INCLUDED
 
-#include "externalConst.h"
+#include "blech.h"
 
-blc_int32 c_name_extVarI8;
-blc_int32 c_name_extVarArr[8];
-blc_01externalConst01_S c_name_S2;
+#define c_name_extVarI8 0
+extern const blc_int32 c_name_extVarArr[8];
 
 #endif
